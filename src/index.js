e2d255467102d533f39c9c78574c742a9718872f
Tanner Linsley <tannerlinsley@gmail.com>
2018-10-08 12:24:55 -0600

    V6 lerna (#804)
    * Move to lerna
    
    * Examples are now packages
    
    * Lerna import react-static-plugin-emotion
    
    * prepend example packages with react-static
    
    * Lerna import react-static-plugin-styled-components
    
    * Lerna import react-static-plugin-sass
    
    * Lerna import react-static-plugin-preact
    
    * Fix: example-contentful package.json version
    
    * Update all example dependencies
    
    * Move babelrc to react-static core
    
    * Move docs to root
    
    * Move repo files back to root
    
    * Fix documentation scripts

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import { deprecate } from './utils/shared'
+import { withRouteData, withSiteData } from './browser/'
+
+//
+
+// React Router Components
+export { Prompt, Route, Switch, matchPath, withRouter } from 'react-router-dom'
+
+// Helmet
+export { Helmet as Head } from 'react-helmet'
+
+// React-Static Components
+export {
+  default as RouteData,
+  withRouteData,
+} from './browser/components/RouteData'
+export {
+  default as SiteData,
+  withSiteData,
+} from './browser/components/SiteData'
+export { default as Loading, withLoading } from './browser/components/Loading'
+export { default as Prefetch } from './browser/components/Prefetch'
+export {
+  default as PrefetchWhenSeen,
+} from './browser/components/PrefetchWhenSeen'
+export { default as Router } from './browser/components/Router'
+export { default as Redirect } from './browser/components/Redirect'
+export { NavLink, Link } from './browser/components/Link'
+
+// Routes
+export { default as Routes } from 'react-static/routes'
+
+// Methods
+export { prefetch, onLoading } from './browser/'
+
+// Public Utils
+export { default as scrollTo } from './utils/scrollTo'
+
+// Private Utils
+export { cleanPath } from './utils/shared'
+
+// Deprecations
+export const getRouteProps = (...args) => {
+  deprecate('getRouteProps', 'withRouteData')
+  return withRouteData(...args)
+}
+export const getSiteData = (...args) => {
+  deprecate('getSiteData', 'withSiteData')
+  return withSiteData(...args)
+}
---
+++ src/static/getConfig.js
@@ -0,0 +1,224 @@
+/* eslint-disable import/no-dynamic-require */
+
+import React from 'react'
+import nodePath from 'path'
+import chokidar from 'chokidar'
+import resolveFrom from 'resolve-from'
+import fs from 'fs-extra'
+
+import getDirname from '../utils/getDirname'
+import {
+  cleanSlashes,
+  cutPathToRoot,
+  isAbsoluteUrl,
+  getPluginHooks,
+} from '../utils/shared'
+
+const DEFAULT_NAME_FOR_STATIC_CONFIG_FILE = 'static.config.js'
+// the default static.config.js location
+const DEFAULT_PATH_FOR_STATIC_CONFIG = nodePath.resolve(
+  nodePath.join(process.cwd(), DEFAULT_NAME_FOR_STATIC_CONFIG_FILE)
+)
+const DEFAULT_ROUTES = [{ path: '/' }]
+const DEFAULT_ENTRY = 'index.js'
+
+export const buildConfigation = (config = {}) => {
+  // path defaults
+  config.paths = {
+    root: nodePath.resolve(process.cwd()),
+    src: 'src',
+    dist: 'dist',
+    temp: 'tmp',
+    devDist: 'tmp/dev-server',
+    public: 'public',
+    plugins: 'plugins',
+    pages: 'src/pages',
+    nodeModules: 'node_modules',
+    assets: '',
+    ...(config.paths || {}),
+  }
+
+  // Use the root to resolve all other relative paths
+  const resolvePath = relativePath =>
+    nodePath.resolve(config.paths.root, relativePath)
+
+  // Resolve all paths
+  const DIST =
+    process.env.REACT_STATIC_ENV === 'development'
+      ? resolvePath(config.paths.devDist || config.paths.dist)
+      : resolvePath(config.paths.dist)
+
+  const ASSETS = nodePath.resolve(DIST, config.paths.assets)
+
+  const paths = {
+    ROOT: config.paths.root,
+    LOCAL_NODE_MODULES: nodePath.resolve(getDirname(), '../../node_modules'),
+    SRC: resolvePath(config.paths.src),
+    PAGES: resolvePath(config.paths.pages),
+    DIST,
+    ASSETS,
+    PLUGINS: resolvePath(config.paths.plugins),
+    TEMP: resolvePath(config.paths.temp),
+    PUBLIC: resolvePath(config.paths.public),
+    NODE_MODULES: resolvePath(config.paths.nodeModules),
+    EXCLUDE_MODULES:
+      config.paths.excludeResolvedModules ||
+      resolvePath(config.paths.nodeModules),
+    PACKAGE: resolvePath('package.json'),
+    HTML_TEMPLATE: nodePath.join(DIST, 'index.html'),
+    STATIC_DATA: nodePath.join(ASSETS, 'staticData'),
+  }
+
+  let siteRoot = ''
+  let basePath = ''
+
+  if (process.env.REACT_STATIC_ENV === 'development') {
+    basePath = cleanSlashes(config.devBasePath)
+  } else if (process.env.REACT_STATIC_STAGING === 'true') {
+    siteRoot = cutPathToRoot(config.stagingSiteRoot, '$1')
+    basePath = cleanSlashes(config.stagingBasePath)
+  } else {
+    siteRoot = cutPathToRoot(config.siteRoot, '$1')
+    basePath = cleanSlashes(config.basePath)
+  }
+
+  const publicPath = `${cleanSlashes(`${siteRoot}/${basePath}`)}/`
+
+  let assetsPath = cleanSlashes(config.assetsPath || config.paths.assets)
+  if (assetsPath && !isAbsoluteUrl(assetsPath)) {
+    assetsPath = `/${cleanSlashes(`${basePath}/${assetsPath}`)}/`
+  }
+
+  // Defaults
+  config = {
+    // Defaults
+    entry: nodePath.join(paths.SRC, DEFAULT_ENTRY),
+    getSiteData: () => ({}),
+    renderToElement: Comp => <Comp />,
+    renderToHtml: (render, comp) => render(comp),
+    prefetchRate: 3,
+    maxThreads: Infinity,
+    disableRouteInfoWarning: false,
+    disableRoutePrefixing: false,
+    outputFileRate: 100,
+    extensions: ['.js', '.jsx'], // TODO: document
+    getRoutes: async () => DEFAULT_ROUTES,
+    plugins: [],
+    // Config Overrides
+    ...config,
+    // Materialized Overrides
+    paths,
+    siteRoot,
+    basePath,
+    publicPath,
+    assetsPath,
+    extractCssChunks: config.extractCssChunks || false,
+    inlineCss: config.inlineCss || false,
+  }
+
+  // Set env variables to be used client side
+  process.env.REACT_STATIC_PREFETCH_RATE = config.prefetchRate
+  process.env.REACT_STATIC_DISABLE_ROUTE_INFO_WARNING =
+    config.disableRouteInfoWarning
+  process.env.REACT_STATIC_DISABLE_ROUTE_PREFIXING =
+    config.disableRoutePrefixing
+
+  const resolvePlugin = plugin => {
+    let resolver = plugin
+    let location = resolver
+    let options = {}
+    if (Array.isArray(plugin)) {
+      resolver = plugin[0]
+      options = plugin[1] || {}
+    }
+    let getHooks = () => ({})
+    // Attempt a direct require for absolute paths
+    try {
+      getHooks = require(resolver).default
+    } catch (err) {
+      try {
+        // Attempt a /plugins directory require
+        location = nodePath.resolve(paths.PLUGINS, resolver)
+        getHooks = require(location).default
+      } catch (err) {
+        // Attempt a root directory require (node_modules)
+        location = resolveFrom(process.cwd(), resolver)
+        getHooks = require(location).default
+      }
+    }
+
+    // remove any `index.js` suffix
+    location = location.replace(/index\.js$/g, '')
+
+    // Build the browser location
+    let browserLocation = nodePath.join(location, 'browser.js')
+    // Detect if the browser plugin entry exists, and provide the location to it
+    browserLocation = fs.pathExistsSync(browserLocation)
+      ? browserLocation
+      : null
+
+    const resolvedPlugin = {
+      resolver,
+      location,
+      browserLocation,
+      options,
+      hooks: getHooks(options) || {},
+    }
+
+    // Recursively resolve plugins
+    if (resolvedPlugin.plugins) {
+      resolvedPlugin.plugins = resolvedPlugin.plugins.map(resolvePlugin)
+    }
+
+    return resolvedPlugin
+  }
+  // Fetch plugins, if any
+  config.plugins = config.plugins.map(resolvePlugin)
+
+  config = getPluginHooks(config.plugins, 'config').reduce(
+    (curr, config) => config(curr),
+    config
+  )
+
+  return config
+}
+
+const buildConfigFromPath = configPath => {
+  const filename = nodePath.resolve(configPath)
+  delete require.cache[filename]
+  try {
+    const config = require(filename).default
+    return buildConfigation(config)
+  } catch (err) {
+    console.error(err)
+    return {}
+  }
+}
+
+const fromFile = (configPath = DEFAULT_PATH_FOR_STATIC_CONFIG, subscribe) => {
+  const config = buildConfigFromPath(configPath)
+
+  if (subscribe) {
+    chokidar.watch(configPath).on('all', () => {
+      subscribe(buildConfigFromPath(configPath))
+    })
+  }
+
+  return config
+}
+
+// Retrieves the static.config.js from the current project directory
+export default (async function getConfig(customConfig, cb) {
+  if (typeof customConfig === 'object') {
+    // return a custom config obj
+    const builtConfig = buildConfigation(customConfig)
+    if (cb) {
+      cb(builtConfig)
+    }
+    return builtConfig
+  }
+
+  // return a custom config file location
+  // defaults to constant paath for static config
+  return fromFile(customConfig, cb)
+})
